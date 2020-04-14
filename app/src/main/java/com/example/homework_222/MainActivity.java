e58a62d83e534a412e4143d9e67abf353e9536d0
GAVR <axotnick@bk.ru>
2020-04-14 14:51:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework_222/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.homework_222;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/homework_222/NotesActivity.java
@@ -0,0 +1,49 @@
+package com.example.homework_222;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private EditText noteEdtx;
+    private Button saveBtn;
+    private SharedPreferences noteSharedPref;
+    private static String NOTE_TEXT = "note_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        noteSharedPref = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        getDateFromSharedPref();
+    }
+
+    private void init() {
+        noteEdtx = findViewById(R.id.noteEdtx);
+        saveBtn = findViewById(R.id.saveBtn);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = noteSharedPref.edit();
+                String noteTxt = noteEdtx.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTxt);
+                myEditor.apply();
+                Toast.makeText(NotesActivity.this, R.string.saveData, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void getDateFromSharedPref() {
+        String noteTxt = noteSharedPref.getString(NOTE_TEXT, "");
+        noteEdtx.setText(noteTxt);
+    }
+}
